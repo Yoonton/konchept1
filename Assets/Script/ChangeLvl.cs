6e36a9579368c407c7736be18166fc3b12a57875
Yoonton <63466663+Yoonton@users.noreply.github.com>
2020-12-05 12:13:42 +0400

    da
    da

---
+++ Assets/Script/ChangeLvl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLvl : MonoBehaviour
+{
+    public GameObject OldLvl;
+    public GameObject NewLvl;
+    void Start()
+    {
+        OldLvl.SetActive(false);
+        NewLvl.SetActive(true);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            OldLvl.SetActive(true);
+            NewLvl.SetActive(false);
+        }
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            OldLvl.SetActive(false);
+            NewLvl.SetActive(true);
+        }
+    }
+}
